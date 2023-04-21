fb1540921b71fb29d6efd3231e7fbad08ed61d4f
Norbert M. Soto <57424278+nsoto92@users.noreply.github.com>
2023-04-21 00:34:09 -0700

    deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Page from '../components/Page/Page';
 import styles from './Home.module.css';
-import { AboutUs } from './sections/AboutUs';
-import { Header } from './sections/Header';
-import { Services } from './sections/Services';
+// import { AboutUs } from './sections/AboutUs';
+// import { Header } from './sections/Header';
+// import { Services } from './sections/Services';
 
 const HomePage = () => {
   return (
     <Page>
       <div className={styles.wrapper}>
-        <Header />
+        <h1>Coming soon... Habla claro!</h1>
+        {/* <Header />
         <AboutUs />
         <Services />
         <h1>Home Page</h1>
@@ -55,7 +56,7 @@ const HomePage = () => {
         <h1>Home Page</h1>
         <h1>Home Page</h1>
         <h1>Home Page</h1>
-        <h1>Home Page</h1>
+        <h1>Home Page</h1> */}
       </div>
     </Page>
   );
